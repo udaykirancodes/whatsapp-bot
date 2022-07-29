d60c23f8ee421196c293eff9b506afe21d685bd9
Udaykiran <officialudaykiran@gmail.com>
2022-07-29 20:34:38 +0530

    New Way

--- index.js
+++ index.js
@@ -63,118 +63,134 @@ const state = {
     groupChats: 0,
     wishes: 0,
     personalChats: 0,
-    notification: 'Connecting To WhatsApp Web'
+    notification: ''
 }
 
 const { Client, RemoteAuth } = require('whatsapp-web.js');
 
-const store = new MongoStore({ mongoose: mongoose });
-const client = new Client({
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true
-    },
-    authStrategy: new RemoteAuth({
-        store: store,
-        backupSyncIntervalMs: 300000
-    })
-});
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('Database Connected');
-
-    client.on('remote_session_saved', () => {
-        console.log('Remote Auth Saved');
-    })
-
-    // Socket IO Starts 
-    io.on('connection', (socket) => {
-        console.log('User Connected');
-        socket.emit('notification', state);
-
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
-        // get & send qr code 
-        client.on('qr', qr => {
-            qrcode.toDataURL(qr, (err, url) => {
-                socket.emit('qrcode', url);
-                state.notification = 'QR Code Received. Scan Please...'
-                socket.emit('notification', state);
+// 1 time client initializer 
+let init = 0;
+const initializer = () => {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
+        if (init === 0) {
+            const store = new MongoStore({ mongoose: mongoose });
+            const client = new Client({
+                puppeteer: {
+                    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                    headless: true
+                },
+                authStrategy: new RemoteAuth({
+                    store: store,
+                    backupSyncIntervalMs: 300000
+                })
+            });
+            client.on('remote_session_saved', () => {
+                console.log('Remote Auth Saved');
             })
-            // qrcode.generate(qr, { small: true });
-        });
-        // send ready status 
-        client.on('ready', () => {
-            state.notification = 'Bot is Ready!';
-            state.connected = true
+            client.initialize();
+            return client;
+        }
+        init++;
+    })
+}
+
+
+
+initializer()
+    .then((client) => {
+        // Socket IO Starts 
+        io.on('connection', (socket) => {
+            console.log('User Connected');
+            state.notification = 'Connecting to WhatsApp Web Bot !!';
             socket.emit('notification', state);
-            console.log('client is ready!');
-        });
+
+            socket.on('disconnect', () => {
+                console.log('user disconnected');
+            });
+            // get & send qr code 
+            client.on('qr', qr => {
+                console.log('QR Code Sent!!')
+                qrcode.toDataURL(qr, (err, url) => {
+                    socket.emit('qrcode', url);
+                    state.notification = 'QR Code Received. Scan Please...'
+                    socket.emit('notification', state);
+                })
+                // qrcode.generate(qr, { small: true });
+            });
+            // send ready status 
+            client.on('ready', () => {
+                state.notification = 'Bot is Ready!';
+                state.connected = true
+                socket.emit('notification', state);
+                console.log('client is ready!');
+            });
 
 
-        client.on('authenticated', (session) => {
-            state.connected = true;
-            state.notification = 'Authenticated Successfully!';
-            socket.emit('authenticated', state);
-            console.log('Authenticated');
-        });
-        client.on('disconnected', (session) => {
-            state.connected = false;
-            state.notification = 'WhatsApp Bot Disconnected!!';
-            socket.emit('notification', state);
-            console.log('disconected');
-        });
+            client.on('authenticated', (session) => {
+                state.connected = true;
+                state.notification = 'Authenticated Successfully!';
+                socket.emit('authenticated', state);
+                console.log('Authenticated');
+            });
+            client.on('disconnected', (session) => {
+                state.connected = false;
+                state.notification = 'WhatsApp Bot Disconnected!!';
+                socket.emit('notification', state);
+                console.log('disconected');
+            });
 
-        client.on('message', async message => {
-            let msg = message.body.toLowerCase();
-            // let chat = await client.getChats()
-            message.reply('hi');
-            if (msg === '❤️' || msg === '🔥' || msg === '⚡') {
-                message.reply(msg);
-            }
-            console.log(msg);
-            // waiting a second to get the latest chat 
-            // setTimeout(() => {
-            //     const start = async () => {
-            //         let chat = await client.getChats()
+            client.on('message', async message => {
+                let msg = message.body.toLowerCase();
+                // let chat = await client.getChats()
+                message.reply('hi');
+                if (msg === '❤️' || msg === '🔥' || msg === '⚡') {
+                    message.reply(msg);
+                }
+                console.log(msg);
+                // waiting a second to get the latest chat 
+                // setTimeout(() => {
+                //     const start = async () => {
+                //         let chat = await client.getChats()
 
-            //         console.log('isGroup :', chat[0].isGroup, 'Msg :', msg)
-            //         if (chat[0].isGroup) {
-            //             state.notification = 'Group Chat Received!!';
-            //             state.groupChats++;
-            //         }
-            //         if (msg.includes('hpy') || msg.includes('hbd') || msg.includes('happy') || msg.includes('birthday') || msg.includes('bornday') || msg.includes('bday')) {
-            //             state.notification = 'Someone Wished you!!';
-            //             state.wishes++;
-            //             chat[0].sendSeen()
-            //             chat[0].sendStateTyping();
-            //         }
-            //         if (!chat[0].isGroup || !chat[0].isForwarded) {
-            state.notification = 'Chat Received!!';
-            //             state.personalChats++;
-            //             send(message, socket);
-            //             chat[0].sendSeen()
-            //             chat[0].sendStateTyping();
-            //             // after 1 second :: send message / reply to the message
-            //         }
-            socket.emit('message', state);
-            sendOut(message, client)
-            //     }
-            //     start();
-            // }, 1000);
-        });
+                //         console.log('isGroup :', chat[0].isGroup, 'Msg :', msg)
+                //         if (chat[0].isGroup) {
+                //             state.notification = 'Group Chat Received!!';
+                //             state.groupChats++;
+                //         }
+                //         if (msg.includes('hpy') || msg.includes('hbd') || msg.includes('happy') || msg.includes('birthday') || msg.includes('bornday') || msg.includes('bday')) {
+                //             state.notification = 'Someone Wished you!!';
+                //             state.wishes++;
+                //             chat[0].sendSeen()
+                //             chat[0].sendStateTyping();
+                //         }
+                //         if (!chat[0].isGroup || !chat[0].isForwarded) {
+                state.notification = 'Chat Received!!';
+                //             state.personalChats++;
+                //             send(message, socket);
+                //             chat[0].sendSeen()
+                //             chat[0].sendStateTyping();
+                //             // after 1 second :: send message / reply to the message
+                //         }
+                socket.emit('message', state);
+                sendOut(message, client)
+                //     }
+                //     start();
+                // }, 1000);
+            });
+
+
+        })
 
     })
-    client.initialize();
-});
 
 
 
 
 
 
-// function to reply 
+
+// seperate function to reply 
 const send = (message, socket) => {
 
     let msg = message.body.toLowerCase();
